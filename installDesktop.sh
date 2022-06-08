aed936a9118d986aa3279d123550a549bf3762a2
ToS <stefan.burger@gmx.com>
2022-06-08 21:54:22 +0200

    Add files via upload

---
+++ installDesktop.sh
@@ -0,0 +1,9 @@
+#prepare install with update first:
+#sudo apt update
+#sudo apt upgrade
+#sudo apt dist-upgrade
+#sudo reboot
+
+sudo apt install xserver-xorg
+sudo apt install raspberrypi-ui-mods
+sudo apt install lightdm
